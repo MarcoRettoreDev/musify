243d6f0dc78ec25b6f8f0a5cec3448b1e4c5b02d
MR.Dev <marcorettoremattes@gmail.com>
2023-02-23 20:52:51 -0300

    Spatie/Media library added and configured

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    public const MEDIA_COLLECTION_AUDIO = 'audio';
+    public const MEDIA_COLLECTION_IMAGE = 'image';
+
+    /**
+     * Add an audio to the model.
+     *
+     * @param \App\Models\BaseModel $model
+     */
+    public static function addAudio($model)
+    {
+        if ($model->hasMedia(self::MEDIA_COLLECTION_AUDIO)) {
+            $model->clearMediaCollection(self::MEDIA_COLLECTION_AUDIO);
+        } else {
+            $model->addMedia()->toMediaCollection(self::MEDIA_COLLECTION_AUDIO);
+        }
+    }
+
+    /**
+     * Add an image to the model.
+     *
+     * @param \App\Models\BaseModel $model
+     */
+    public static function addImage($model)
+    {
+        if ($model->hasMedia(self::MEDIA_COLLECTION_IMAGE)) {
+            $model->clearMediaCollection(self::MEDIA_COLLECTION_IMAGE);
+        } else {
+            $model->addMedia()->toMediaCollection(self::MEDIA_COLLECTION_IMAGE);
+        }
+    }
+}
